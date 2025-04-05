fb99f89754334cbbaffd20914d99e57df656741b
galpavol22 <134165885+galpavol22@users.noreply.github.com>
2025-04-05 21:12:37 +0200

    xa

--- hotel_booking/settings.py
+++ hotel_booking/settings.py
@@ -7,7 +7,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'your-secret-key-here'
 
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+
 
 ALLOWED_HOSTS = ['*']
 
