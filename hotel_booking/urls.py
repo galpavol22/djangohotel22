97bb04a2f42e6cb1c1e2414a3e9579ecea671ed6
galpavol22 <134165885+galpavol22@users.noreply.github.com>
2025-04-05 21:28:47 +0200

    ucet

--- hotel_booking/urls.py
+++ hotel_booking/urls.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.contrib.auth import get_user_model
+from django.db.utils import OperationalError
+
+try:
+    User = get_user_model()
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser("admin", "admin@example.com", "Admin123")
+        print("Admin account created.")
+except OperationalError:
+    pass
 
 urlpatterns = [
     path('admin/', admin.site.urls),
